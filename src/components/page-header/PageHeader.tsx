f2eef8656f45f94530b47b4c994d18458b6b6b53
Marco Vignati <marcovignatidev@gmail.com>
2023-09-22 22:33:11 -0400

    add active link classname

--- src/components/page-header/PageHeader.tsx
+++ src/components/page-header/PageHeader.tsx
@@ -1,14 +1,23 @@
+'use client'
+
 import './PageHeader.scss';
 
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+
 export default function PageHeader() {
+
+  const pathname = usePathname();
+
   return (
     <header className="header">
       <span>marco-vignati</span>
       <nav className="flex">
-        <li><a href="/">_hello</a></li>
-        <li><a href="/about-me">_about-me</a></li>
-        <li><a href="/projects" className="projects">_projects</a></li>
-        <li><a href="/contact-me" className="contact">_contact-me</a></li>
+        <li><Link className={pathname == "/" ? "active" : ""} href="/">_hello</Link></li>
+        <li><Link className={pathname == "/about-me" ? "active" : ""} href="/about-me">_about-me</Link></li>
+        <li><Link className={pathname == "/projects" ? "active" : ""} id="projects" href="/projects">_projects</Link></li>
+        <li><Link className={pathname == "/contact-me" ? "active" : ""} id="contact" href="/contact-me">_contact-me</Link></li>
       </nav>
     </header>
   )
