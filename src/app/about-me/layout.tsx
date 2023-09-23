551bed722167b49ff2a92299cf2f0bd2b60b786b
Marco Vignati <marcovignatidev@gmail.com>
2023-09-23 14:36:36 -0400

    changed folder colors

--- src/app/about-me/layout.tsx
+++ src/app/about-me/layout.tsx
@@ -1,6 +1,5 @@
 import './about.scss'
 
-import Folder from '@/components/icons/folders/Folder'
 import Folder1 from '@/components/icons/folders/Folder1'
 import Folder2 from '@/components/icons/folders/Folder2'
 import Folder3 from '@/components/icons/folders/Folder3'
@@ -13,9 +12,9 @@ export default function AboutLayout({
   return (
     <section className='about_page'>
       <aside className='about_aside'>
-        <a href="/about-me/bio"><Folder /> bio</a>
-        <a href="/about-me/interests"><Folder /> interests</a>
-        <a href="/about-me/education"><Folder /> education</a>
+        <a href="/about-me/bio"><Folder1 /> bio</a>
+        <a href="/about-me/interests"><Folder2 /> interests</a>
+        <a href="/about-me/education"><Folder3 /> education</a>
       </aside>
       <section>
         {children}
